52c588f23afdf32e86dd04b9f43c306c8f9d0bd9
Майрбек  Мадиев <majrbekmadiev@MacBook-Air-Majrbek.local>
2025-05-31 17:07:23 +0200

    Made The Card component dynamic

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
+import React, { useEffect } from "react";
+import { useState } from "react";
 import Overlay from "./components/Overlay";
 import Header from "./components/Header";
 import Card from "./components/Card";
 
 
+
 const App = () => {
+  const [sneakers, setSneakers] = useState([]);
+
+  useEffect(() => {
+    fetch('../public/db.json')
+      .then((data) => data.json())
+      .then((json) => {
+        console.log(json)
+        setSneakers(json.items)
+      })
+      .catch((err) => {
+        console.warn(err)
+        alert('Something went wrong')
+      })
+  },[])
+
   return (
     <>
       <div className="wrapper clear">
@@ -17,8 +35,12 @@ const App = () => {
               <input placeholder="Поиск..." />
             </div>
           </div>
-          <div className="d-flex">
-            <Card/>
+          <div className="d-flex flex-wrap">
+
+            {
+              sneakers.map((sneaker) => <Card key={sneaker.id} {...sneaker}/>)
+            }
+            
           </div>
         </div>
       </div>
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
 
-const Card = () => {
+const Card = ({title, price, imageUrl}) => {
   return (
-    <div className="card">
+    <div className="card mb-20">
         <div className="favorite">
             <img src="/img/unliked.svg" alt="heart" />
         </div>
-        <img width={133} height={112} src="/img/sneakers/1.jpg" alt="1" />
-        <h5>Мужские Кроссовки Nike Biazer Mid Suede</h5>
+        <img width={133} height={112} src={imageUrl} alt="sneakers" />
+        <h5>{title}</h5>
         <div className="d-flex justify-between align-center">
             <div className="d-flex flex-column">
                 <span>Цена</span>
-                <b>12 999 руб.</b>
+                <b>{price} руб.</b>
             </div>
         <button className="button">
             <img src="/img/plus.svg" alt="plus" />
